b05b3a606c229fd76800fec9df8646dd9038421f
Koji A <koji@usernombp.local>
2018-09-02 23:55:28 +0900

    Initial Commit

---
+++ ImageDetector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImageDetector
+//
+//  Created by Koji A on 2018/09/02.
+//  Copyright © 2018年 Koji A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
